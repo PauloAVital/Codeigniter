162bd4070ebd1e48f594d3f76ed244241e8fa51e
Paulo A. Vital <pauloavital@gmail.com>
2020-02-01 11:33:31 -0300

    Iniciando o Projeto - Estudos em casa

---
+++ application/libraries/Template.php
@@ -0,0 +1,15 @@
+<?php if( ! defined('BASEPATH')) exit('no direct script access allowed');
+
+class Template {
+    function show($view, $data=array()) {
+        $CI = &get_instance();
+
+        $CI->load->view('template/header', $data);
+
+        $CI->load->view($view, $data);
+
+        $CI->load->view('template/footer', $data);
+
+        $CI->load->view('template/scripts', $data);
+    }
+}
---
+++ application/views/restrict.php
@@ -0,0 +1,3 @@
+<p>
+Area Restrita
+</p>
